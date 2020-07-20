9d92ef11d43cae0e3b30736847dbb1e49a6eab70
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-20 21:51:30 +0530

    Percentage Calculation for Doublet is added

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -7,12 +7,12 @@ flipnum=40
 for((i=0; i<$flipnum; i++))
 do
    coin=$((RANDOM%2))
-   if [ $coin -eq 0 ]
-   then
-      ((Singlet[H]++))
-   else
-      ((Singlet[T]++))
-   fi
+	case $coin in
+      0)
+         ((Singlet[H]++)) ;;
+      1)
+         ((Singlet[T]++)) ;;
+	esac
 done
 echo "Percentage of Singlet Combination are: "
 for key in ${!Singlet[@]}
@@ -21,3 +21,26 @@ do
 	echo "$key : ${Singlet[$key]}"
 done
 
+
+declare -A Doublet=( [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
+
+for((i=0; i<$flipnum; i++))
+do
+   coin=$((RANDOM%4))
+   case $coin in 
+   	0)
+      	((Doublet[HH]++)) ;;
+		1)
+      	((Doublet[HT]++)) ;;
+		2)
+			((Doublet[TH]++)) ;;
+		3)
+			((Doublet[TT]++)) ;;
+	esac
+done
+echo "Percentage of Doublet Combination are: "
+for key in ${!Doublet[@]}
+do
+   Doublet[$key]=`printf %.2f "$(( (${Doublet[$key]})*100*100/$flipnum ))e-2"`
+   echo "$key : ${Doublet[$key]}"
+done
