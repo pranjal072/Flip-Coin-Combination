77bb319422e93846451c81c42ec3f8e70d873a5f
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-20 21:38:55 +0530

    Percentage Calculation for Singlet is added

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,23 @@
-#!/bin/bash -x 
+#!/bin/bash
 
 echo "Flip Coin Combination:"
 
-coin=$((RANDOM%2))
-if [ $coin -eq 0 ]
-then
-	echo "Heads Wins"
-else
-	echo "Tails Wins"
-fi
+declare -A Singlet=( [H]=0 [T]=0 )
+flipnum=40
+for((i=0; i<$flipnum; i++))
+do
+   coin=$((RANDOM%2))
+   if [ $coin -eq 0 ]
+   then
+      ((Singlet[H]++))
+   else
+      ((Singlet[T]++))
+   fi
+done
+echo "Percentage of Singlet Combination are: "
+for key in ${!Singlet[@]}
+do
+	Singlet[$key]=`printf %.2f "$(( (${Singlet[$key]})*100*100/$flipnum ))e-2"`
+	echo "$key : ${Singlet[$key]}"
+done
+
